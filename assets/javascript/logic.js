91b56b460732d64efa4f562677738d5064251bc7
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-03-26 17:12:25 -0400

    3/26/2016

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
 var likes = 0;
 var totalPhotos = 0;
 
+
+var pictures = document.body.getElementsByTagName("img");
+for (var i = 0; i < pictures.length; i++) {
+	console.log(pictures);
+	totalPhotos++;
+	console.log(totalPhotos);
+	$(".pictures").html(totalPhotos);
+
+}
+
+//click function for adding likes//
 $("button").click(function() {
 		likes++;
 		console.log(likes);
