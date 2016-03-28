16b03b804cd4e307bed9c539cb9943c254911ad2
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-03-28 13:09:46 -0400

    3/28/2016

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     var likes = 0;
+    // console.log(likes);
     var totalPhotos = 0;
     // click function for adding likes//
-    $("button").one("click", function() {
-        likes++;
-        $(".like").html(likes);
-        console.log(likes);
-        var index = $("button").index(this);
-        $(this).css({
-            color: "black",
-            background: "#3BFF34",
-            border: "none",
-            cursor: "auto"
+    function addLikes() {
+        $(".btn-danger").one("click", function() {
+            likes++;
+            $(".like").html(likes);
+            var index = $(".btn").index(this);
+            $(this).css({
+                color: "black",
+                background: "#3BFF34",
+                border: "none",
+                cursor: "auto"
+            });
         });
-    });
+    };
+    addLikes();
     //find total pictures in the document and display the number
     var pictures = document.body.getElementsByTagName("img");
     for (var i = 0; i < pictures.length; i++) {
@@ -23,7 +26,7 @@ $(document).ready(function() {
         $(".pictures").html(totalPhotos);
     };
     //database for stroing likes
-    var dataBase = new Firebase("https://wildgallery.firebaseio.com/");
+    var dataBase = new Firebase("https://wildgallery.firebaseio.com");
     dataBase.set({
         likes: likes,
         pictures: totalPhotos
