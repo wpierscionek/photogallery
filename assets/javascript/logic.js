225929b529959ba2205e1822232973d2c9f25fc6
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-03-30 11:01:59 -0400

    3/30/2016

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,42 +1,39 @@
-$(document).ready(function() {
-    var likes = 0;
-    // console.log(likes);
-    var totalPhotos = 0;
-    // disables right click on pictures
-    $("img").bind("contextmenu", function() {
-        return false;
-    });
-    // click function for adding likes//
-    function addLikes() {
-        $(".btn-danger").one("click", function() {
-            likes++;
-            $(".like").html(likes);
-            var index = $(".btn").index(this);
-            $(this).css({
-                color: "black",
-                background: "#3BFF34",
-                border: "none",
-                cursor: "auto"
-            });
-        });
-    };
-    addLikes();
-    //find total pictures in the document and display the number
-    var pictures = document.body.getElementsByClassName("example-image");
-    for (var i = 0; i < pictures.length; i++) {
-        // console.log(pictures);
-        totalPhotos++;
-        // console.log(totalPhotos);
-        $(".pictures").html(totalPhotos);
-    };
-    //database for stroing likes
-    var dataBase = new Firebase("https://wildgallery.firebaseio.com");
+//creating firebase
+var dataBase = new Firebase("https://wildgallery.firebaseio.com");
+//global variables
+var likes = 0;
+var totalPhotos = 0;
+//on click function for adding likes
+$(".btn-danger").one("click", function() {
+    likes++;
+    // saving data to firebase
     dataBase.set({
         likes: likes,
-        pictures: totalPhotos
+        photos: totalPhotos
     });
-    dataBase.on("value", function(snapshot) {
-        $(".likes").html(snapshot.val().likes);
-        $(".pictures").html(snapshot.val().totalPhotos);
+    //on click change btn color
+    var index = $(".btn").index(this);
+    $(this).css({
+        color: "black",
+        background: "#3BFF34",
+        border: "none",
+        cursor: "auto"
     });
+});
+//disables right click on pictures
+$("img").bind("contextmenu", function() {
+    return false;
+});
+//find total pictures in the document and display the number
+var pictures = document.body.getElementsByClassName("example-image");
+for (var i = 0; i < pictures.length; i++) {
+    totalPhotos++;
+    // $(".pictures").html(totalPhotos);
+};
+//retrieving data from firebase 
+dataBase.on("value", function(snapshot) {
+    likes = snapshot.val().likes;
+    totalPhotos = snapshot.val().photos;
+    $(".like").html(snapshot.val().likes);
+    $(".pictures").html(snapshot.val().photos);
 });
